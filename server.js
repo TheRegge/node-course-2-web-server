c5c2e35da094535ba3d05f6ef6d7d927d40aefd0
Regis Zaleman <regis.zaleman@gmail.com>
2018-10-24 11:53:55 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+// MIDDLEWARE:
+// Site under maintenance
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+// MIDDLEWARE:
+// Log server requests
+app.use((req, res, next) => {
+    // Call next() when done
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// MIDDLEWARE:
+// Serve a static dir
+app.use(express.static(__dirname+'/public'));
+
+// Handlebar template helper
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+// Handlebar template helper
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// Route
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Regis',
+    //     likes: ['motorcycles', 'good food', 'you know, I\'m french']
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome to my website'
+    });
+});
+
+// Route
+app.get('/about', (req, res) => {
+    // res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+// Route
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle the request'
+    });
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
